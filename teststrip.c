ddfe4357e7b1eca444a26dcf6ec4a002213a8a9b
sb <sb>
2003-11-09 20:59:46 +0000

    Read the input file into a memory buffer, and call the new stripHtml() document with this memory buffer, and using "-" (ie. stdout) as the output file name.

--- teststrip.c
+++ teststrip.c
@@ -1,13 +1,26 @@
 #include "striphtml.h"
-#include <libxml/HTMLparser.h>
+#include <sys/stat.h>
 
 main(int argc, char** argv)
 {
   if (argc>1) {
+    int htmlFileSize, retcode, noOfBytesRead;
+    char* htmlFileBuf;
+    struct stat stbuf;
+    FILE* htmlFile;
     const char* htmlFileName = argv[1];
-    xmlDocPtr htmlDoc = htmlParseFile(htmlFileName, 0);
-    xmlDocPtr strippedDoc = stripHtml(htmlDoc);
-    /* save document to stdout */
-    htmlSaveFile("-", strippedDoc);
+    /* Read the file into a memory buffer */
+    retcode = stat(htmlFileName, &stbuf);
+    fprintf(stderr, "stat retcode: %d\n", retcode);
+    htmlFileSize = stbuf.st_size;
+    fprintf(stderr, "input file size: %d\n", htmlFileSize);
+    htmlFileBuf = malloc(htmlFileSize+1);
+    htmlFile = fopen(htmlFileName, "r");
+    noOfBytesRead = fread(htmlFileBuf, 1, htmlFileSize, htmlFile);
+    fprintf(stderr, "input file bytes read: %d\n", noOfBytesRead);
+    htmlFileBuf[htmlFileSize] = '\000';
+
+    /* Stripped HTML document and write the stripped doc to stdout */
+    stripHtml(htmlFileBuf, htmlFileSize, "-");
   }
 }
