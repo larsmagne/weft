9318672c694e1f158b4f91fc19650f8bccee924a
larsi <larsi>
2005-01-01 05:12:45 +0000

    utf8.

--- transform.h
+++ transform.h
@@ -3,6 +3,7 @@
 
 void transform_file(const char *input_file_name, 
 		    const char *output_file_name);
+char *convert_to_utf8(const char *string, const char *charset);
 
 extern const char *message_id;
 
