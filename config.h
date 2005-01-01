6fdba782292058299bf85c6c674b7075173a62c7
larsi <larsi>
2005-01-01 05:10:58 +0000

    Max length for regexp.

--- config.h
+++ config.h
@@ -5,5 +5,6 @@
 #define SPOOL_DIRECTORY "/mnt/var/spool/news/articles"
 #define PICON_DIRECTORY "/usr/lib/picons"
 #define MAX_PLAIN_LINE_LENGTH 90
+#define MAX_TEXT_PLAIN_LENGTH (50*1024)
 
 #endif
