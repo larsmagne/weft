faa4bbea2aeeb5b08e5cd9f7087372551b83ae17
larsi <larsi>
2005-03-25 01:29:10 +0000

    Export.

--- transform.h
+++ transform.h
@@ -4,6 +4,8 @@
 void transform_file(const char *input_file_name, 
 		    const char *output_file_name);
 char *convert_to_utf8(const char *string, const char *charset);
+void transform_part(FILE *output, const char *output_file_name,
+		    GMimeObject *mime_part);
 
 extern const char *message_id;
 
